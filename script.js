ff7efd864781391ced4b4f4dc41aeebe92b0f6df
vlaaaaaaaaaaaaaaad <130173359+vlaaaaaaaaaaaaaaad@users.noreply.github.com>
2023-04-09 18:10:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 function checkPassword() {
     var password = document.getElementById("password").value;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var passwords = this.responseText.split("\n");
-            if (passwords.includes(password)) {
-                window.location.href = "https://gpt-4-chat-ui.vladantonienko.repl.co/";
-            } else {
-                document.getElementById("error-message").innerHTML = "Неверный пароль, попробуйте еще раз!";
+    var errorMessage = document.getElementById("error-message");
+    if (password === "") {
+        errorMessage.innerText = "Пожалуйста, введите пароль";
+        return false;
+    } else {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var passwords = this.responseText.split("\n");
+                if (passwords.includes(password)) {
+                    window.location.href = "https://gpt-4-chat-ui.vladantonienko.repl.co/";
+                } else {
+                    errorMessage.innerText = "Неверный пароль, попробуйте еще раз!";
+                }
             }
-        }
-    };
-    xhttp.open("GET", "passwords.txt", true);
-    xhttp.send();
-}
-if (password === "") {
-    errorMessage.innerText = "Пожалуйста, введите пароль";
-    return false;
-  } else {
-    errorMessage.innerText = "";
-    // здесь можно добавить код для проверки пароля
-    // и переадресации пользователя на другую страницу
-  }
+        };
+        xhttp.open("GET", "passwords.txt", true);
+        xhttp.send();
+    }
 }
